5636bcc7d5adc05fdc8528cfdded24e9a6cfe73a
Mikael Lindqvist <li.mikael@gmail.com>
2021-09-25 15:47:39 +0800

    layout

--- src/view/InventoryView.jsx
+++ src/view/InventoryView.jsx
@@ -17,7 +17,7 @@ export default function InventoryView(props) {
 		);
 	}
 
-	let cls="adv-bx bg-body text-warning adv-inventory";
+	let cls="adv-bx bg-body text-primary adv-inventory";
 	if (props.state.currentVerb)
 		cls+=" adv-verb-selected";
 
