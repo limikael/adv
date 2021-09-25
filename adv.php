9b89dde295680c2fdafc2f394157ff01f25c7c88
Mikael Lindqvist <li.mikael@gmail.com>
2021-09-25 15:43:03 +0800

    version

--- adv.php
+++ adv.php
@@ -5,7 +5,7 @@
  * Plugin Name:       adv
  * Plugin URI:        https://github.com/limikael/adv
  * Description:       Adventure Game.
- * Version:           0.0.3
+ * Version:           0.0.4
  */
 
 defined( 'ABSPATH' ) || exit;
