c8002d46182264060535fb34fd2bd2a0e4c4ae75
Mikael Lindqvist <li.mikael@gmail.com>
2021-09-27 13:33:56 +0800

    admin content editor size

--- inc/tpl/admin-content.tpl.php
+++ inc/tpl/admin-content.tpl.php
@@ -1,2 +1,2 @@
 <input type="hidden" name="adv-content" value="<?php echo esc_attr($content); ?>"></input>
-<div id="editor-adv-content" style="width: 100%; height: 50vh"></div>
+<div id="editor-adv-content" style="width: 100%; height: 55vh"></div>
